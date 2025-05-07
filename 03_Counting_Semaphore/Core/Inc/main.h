8ca18535506cb55b715dcec5dcdd7116891ba42a
moez.meftah <moez.meftah@celadodc-rswl.com>
2025-05-07 14:57:19 +0100

    feat: add counting semaphore example for managing resource access

---
+++ 03_Counting_Semaphore/Core/Inc/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f4xx_hal.h"
+
+void MX_GPIO_Init(void);
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+#endif
